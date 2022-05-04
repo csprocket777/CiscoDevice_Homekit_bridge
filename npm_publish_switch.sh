47b73c3e6f7663c97f63bdad3d90e2c8e2f2a967
Chuck Shipman <chshipma@cisco.com>
2022-05-04 14:13:33 -0500

    fix(publishing): make the npm_publish_switch.sh script work for both OsX and linux

--- npm_publish_switch.sh
+++ npm_publish_switch.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 filename="package.json"
-
+SEDOPTION=
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SEDOPTION="-i ''"
+fi
+# if [[ "$OSTYPE" == "darwin"* ]]; then 
+#     SEDOPTION='-i \x27\x27'; 
+# else 
+#     SEDOPTION='-i'; 
+# fi;
 
 search="https:\/\/hr-tm-cms-git.cisco.com\/collaboration_device_macros\/ciscodevice_homekit_bridge.git"
 replace="https:\/\/github.com\/csprocket777\/CiscoDevice_Homekit_bridge.git"
-sed -i "" "s/$search/$replace/g" $filename
+sed $SEDOPTION "s/$search/$replace/g" $filename
 
 
 search="@collaboration_device_macros:registry"
 replace="registry"
-sed -i "" "s/$search/$replace/g" $filename
+sed $SEDOPTION "s/$search/$replace/g" $filename
 
 
 search="https:\/\/hr-tm-cms-git.cisco.com\/api\/v4\/projects\/122\/packages\/npm\/"
 replace="https:\/\/registry.npmjs.org\/"
-sed -i "" "s/$search/$replace/g" $filename
+sed $SEDOPTION "s/$search/$replace/g" $filename
 
 
 search="@collaboration_device_macros\/homebridge-cisco-collaboration-devices"
 replace="homebridge-cisco-collaboration-devices"
-sed -i "" "s/$search/$replace/g" $filename
+sed $SEDOPTION "s/$search/$replace/g" $filename
