c3b3b5a75826712f72420f1886528479d5d871c9
Chuck Shipman <chshipma@cisco.com>
2022-05-04 19:16:40 -0500

    wip: ensure the build happens in the npmjs_publish folder

--- npmjs_publish_filemove.sh
+++ npmjs_publish_filemove.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -R ./dist/ ./npmjs_publish/dist/
+cp -R ./src/ ./npmjs_publish/src/
 cp ./README.md ./npmjs_publish/README.md
+cp ./tsconfig.json ./npmjs_publish/tsconfig.json
 cp ./config.schema.json ./npmjs_publish/config.schema.json
 cp ./CHANGELOG.md ./npmjs_publish/CHANGELOG.md
